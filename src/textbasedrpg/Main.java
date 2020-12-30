6669e767aea2e501ed18d33c75d578a298c3153e
Robert W. Simons <46572797+Robb-Simons@users.noreply.github.com>
2020-12-29 22:26:56 -0500

    Add program header

--- src/textbasedrpg/Main.java
+++ src/textbasedrpg/Main.java
@@ -1,5 +1,10 @@
 package textbasedrpg;
-
+/*************************************************************************************************
+ * Written by: Robert Simons 
+ * Composition date: 12-29-2020
+ * Program name: Text-based RPG project
+ * Program Desc:                                         
+ *************************************************************************************************/
 import java.util.Random;
 import java.util.Scanner;
 
--- src/textbasedrpg/Monster.java
+++ src/textbasedrpg/Monster.java
@@ -1,5 +1,10 @@
 package textbasedrpg;
-
+/*************************************************************************************************
+ * Written by: Robert Simons 
+ * Composition date: 12-29-2020
+ * Program name: Text-based RPG project
+ * Program Desc:                                         
+ *************************************************************************************************/
 public class Monster {
 	private int maxEnemyHealth = 75;
 	private int EnemyAttack = 25;
