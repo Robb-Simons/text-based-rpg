f9f7f41dca04735d3778da5d6037a1b70c34bbff
Robert W. Simons <46572797+Robb-Simons@users.noreply.github.com>
2021-01-01 22:50:27 -0500

    Update Combat.java

--- src/textbasedrpg/Combat.java
+++ src/textbasedrpg/Combat.java
@@ -15,17 +15,38 @@ public class Combat {
 	Player Player = new Player();
 	
 	public void startCombat() {
-		//this method should start the combat block, while in this 
+		// this method should start the combat block, while in this
 		int enemyHealth = rand.nextInt(Monster.getMaxEnemyHealth());
 		String enemyName = Monster.RandomEnemy();
 		System.out.println("\t!#> " + enemyName + " has appeared! <#! ");
-		
-		//While enemy is alive
+
+		// While enemy is alive
 		while (enemyHealth > 0) {
-			System.out.println(Player.getPlayerName()+"\n     HP: "+ Player.getPlayerHealth() + "     Stamina: " + Player.getPlayerStamina());
+			System.out.println(Player.getPlayerName() + "\n     HP: " + Player.getPlayerHealth() + "     Stamina: "
+					+ Player.getPlayerStamina());
 			System.out.println("----------------------------");
-			System.out.println(enemyName +"\n     HP: "+ enemyHealth);
-			
+			System.out.println(enemyName + "     HP: " + enemyHealth);
+
+			Scanner in = new Scanner(System.in);
+			System.out.println("\n\t # ACTIONS # ");
+			System.out.println("\t1.) Attack!");
+			System.out.println("\t2.) Block!");
+			System.out.println("\t3.) Run!\n");
+			System.out.println("What are you going to do, " + Player.getPlayerName() + "?");
+			int input = in.nextInt();
+			//Attack
+			if (input == 1) {
+
+			}
+			//Block
+			else if (input == 2) {
+
+			}
+			//Run chance
+			if (input == 3) {
+
+			}
+
 			enemyHealth = 0;
 		}
 	}
